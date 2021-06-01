3170c81625756e01b3f2a83a03c2127046c0dd80
Raihanul Haque <raihan.hd.500@gmail.com>
2021-06-01 16:03:16 +0600

    create, delete, get all blogs routes setup

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-require('./db/mongoose')
+require("./db/mongoose");
 const bodyParser = require("body-parser");
 
 const app = express();
@@ -7,21 +7,20 @@ const app = express();
 // PORT
 const PORT = process.env.PORT || 4000;
 
-const cors = require('cors')
-app.use(cors({origin: '*'}))
+const cors = require("cors");
+app.use(cors({ origin: "*" }));
 
-app.use(express.json())
+app.use(express.json());
 
 const authRoutes = require("./routes/auth");
+const blogPostRoutes = require("./routes/blogPost");
 app.use("/oiiu", authRoutes);
-
+app.use("/oiiu", blogPostRoutes);
 
 app.get("/", (req, res) => {
   res.json({ message: "API Working…" });
 });
 
-
 app.listen(PORT, (req, res) => {
   console.log(`Server Started at PORT ${PORT}`);
 });
-
---
+++ src/models/blogPost.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const blogPostSchema = new mongoose.Schema(
+  {
+    description: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    likes: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+      }
+    ],
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const BlogPost = mongoose.model('BlogPost', blogPostSchema);
+
+module.exports = BlogPost;
